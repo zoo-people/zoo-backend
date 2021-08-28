ebb334e5ed36f6cc0efd243a3a589b7a5e661da5
Justin W Soto <soto@pdx.edu>
2021-08-28 12:42:56 -0700

    added forest species data

---
+++ data/forest.js
@@ -0,0 +1,76 @@
+module.export = [
+    {
+        name: 'Beaver',
+        species_name: '(Castor canadensis)',
+        habitat: 'Evergreen Forest',
+        image_url: 'https://en.wikipedia.org/wiki/North_American_beaver#/media/File:American_Beaver.jpg',
+        icon_url: '',
+        description: `The beaver is the largest semiaquatic rodent in North America. The beaver possesses 
+        continuously (or endlessly) growing incisors. Beavers are active mainly at night. They are excellent 
+        swimmers and may remain submerged up to 15 minutes. More vulnerable on land, they tend to remain in 
+        the water as much as possible. They construct their homes, or "lodges", out of sticks, twigs, rocks, 
+        and mud in lakes, streams, and tidal river deltas. The purpose of the dam is to create deepwater refugia 
+        enabling the beaver to escape from predators. `,
+        diet: 'Herbivore'
+        },
+        {
+        name: 'Brown Bear',
+        species_name: '(Ursus arctos)',
+        habitat: 'Evergreen Forest',
+        image_url: 'https://en.wikipedia.org/wiki/Brown_bear#/media/File:Ursus_arctos_-_Norway.jpg',
+        icon_url: '',
+        description: `The brown bear is a large bear species found across Eurasia and North America.
+        In North America, the populations of brown bears are called grizzly bears, while the subspecies
+        that inhabits the Kodiak Islands of Alaska is known as the Kodiak bear. It is one of the largest 
+        living terrestrial members of the order Carnivora, rivaled in size only by its closest relative, the polar bear`,
+        diet: 'Omnivore'
+        },
+        {
+        name: 'Chipmunk',
+        species_name: '(Tamias striatus)',
+        habitat: 'Evergreen Forest',
+        image_url: 'https://en.wikipedia.org/wiki/Chipmunk#/media/File:Tamia_striatus_eating.jpg',
+        icon_url: '',
+        description: `Chipmunks are small, striped rodents of the family Sciuridae. Chipmunks are found in North America, 
+        with the exception of the Siberian chipmunk which is found primarily in Asia. These small mammals fulfill several 
+        important functions in forest ecosystems. Their activities harvesting and hoarding tree seeds play a crucial role
+        in seedling establishment. They consume many different kinds of fungi, including those involved in symbiotic 
+        mycorrhizal associations with trees, and are an important vector for dispersal of the spores of subterranean 
+        sporocarps (truffles) which have co-evolved with these and other mycophagous mammals and thus lost the ability 
+        to disperse their spores through the air.`,
+        diet: 'Omnivore'
+        },
+        {
+        name: 'Moose',
+        species_name: '(Alces alces)',
+        habitat: 'Evergreen Forest',
+        image_url: 'https://en.wikipedia.org/wiki/Moose#/media/File:Moose_983_LAB.jpg',
+        icon_url: '',
+        description: `The moose (in North America) or elk (in Eurasia) (Alces alces), is a member of the New World deer 
+        subfamily and is the largest and heaviest extant species in the deer family. Most adult male moose have distinctive
+        broad, palmate ("open-hand shaped") antlers; most other members of the deer family have antlers with a dendritic 
+        ("twig-like") configuration. Moose are cold-adapted mammals with thickened skin, dense, heat-retaining coat, and a low surface:volume 
+        ratio, which provides excellent cold tolerance but poor heat tolerance. Moose survive hot weather by accessing shade or cooling wind, 
+        or by immersion in cool water. In hot weather, moose are often found wading or swimming in lakes or ponds.   Although generally slow-moving 
+        and sedentary, moose can become aggressive, and move quickly if angered or startled. Their mating season in the autumn features 
+        energetic fights between males competing for a female.`,
+        diet: 'Herbivore'
+        },
+        {
+        name: 'Wolf',
+        species_name: '(Canis lupus)',
+        habitat: 'Evergreen Forest',
+        image_url: 'https://en.wikipedia.org/wiki/Wolf#/media/File:Front_view_of_a_resting_Canis_lupus_ssp.jpg',
+        icon_url: '',
+        description: `Of all members of the genus Canis, the wolf is most specialized for cooperative game hunting as demonstrated by its physical 
+        adaptations to tackling large prey, its more social nature, and its highly advanced expressive behaviour. It travels in nuclear families 
+        consisting of a mated pair accompanied by their offspring. Offspring may leave to form their own packs on the onset of sexual maturity and 
+        in response to competition for food within the pack. Wolves are also territorial and fights over territory are among the principal causes of wolf mortality.
+        Thewolf is also considered the ancestor of most domestic dog breeds. Although the fear of wolves exists in many human societies, the majority of recorded 
+        attacks on people have been attributed to animals suffering from rabies. Wolf attacks on humans are rare because wolves are relatively few, live away from 
+        people, and have developed a fear of humans because of their experiences with hunters, ranchers, and shepherds.`,
+        diet: 'Carnivore'
+        }
+];
+    
+    
