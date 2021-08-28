acc2fc73bb835fb7a313a5d59893946e700d9eff
Joseph-K-B <303josephb@gmail.com>
2021-08-27 20:14:09 -0700

    basic data structure

---
+++ data/desert.js
@@ -0,0 +1,48 @@
+modules.export = [
+  { id: 1,
+    name: 'Camel',
+    species_name: 'Camelus ferus',
+    habitat: 'Desert',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Wild_Bactrian_camel_on_road_east_of_Yarkand.jpg/440px-Wild_Bactrian_camel_on_road_east_of_Yarkand.jpg',
+    icon_url: '',
+    description: '',
+    diet: a'herbivore' },
+
+  { id: 2,
+    name: 'Sand Cat',
+    species_name: 'Felis Margarita',
+    habitat: 'Desert',
+    image_url: '',
+    icon_url: '',
+    description: '',
+    diet: 'omnivore' },
+
+
+  { id: 3,
+    name: 'Desert Hedgehog',
+    species_name: 'Paraechinus aethiopicus',
+    habitat: 'Desert',
+    image_url: '',
+    icon_url: '',
+    description: '',
+    diet: 'carnivore' },
+
+  { id: 4,
+    name: 'Meerkats',
+    species_name: 'Suricata suricatta',
+    habitat: 'Desert',
+    image_url: '',
+    icon_url: '',
+    description: '',
+    diet: 'omnivore' },
+    
+  { id: 5,
+    name: 'Kalahari Lion',
+    species_name: 'Camelus ferus',
+    habitat: 'Desert',
+    image_url: '',
+    icon_url: '',
+    description: '',
+    diet: '' },
+        
+];
