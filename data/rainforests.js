08845c1fbe9d967f709fdbb9340ace54d7b31a1b
Joseph-K-B <303josephb@gmail.com>
2021-08-28 19:04:03 -0700

    altered database to habitat_id

--- data/rainforests.js
+++ data/rainforests.js
@@ -1,7 +1,7 @@
 module.exports = [{
   name: 'Mountain Gorilla',
   species_name: 'Gorilla beringei beringei',
-  habitat: 'Rainforests',
+  habitat: 4,
   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Susa_group%2C_mountain_gorilla.jpg/220px-Susa_group%2C_mountain_gorilla.jpg',
   icon_url: 'https://image.flaticon.com/icons/png/128/1063/1063455.png',
   description: 'The fur of the mountain gorilla, often thicker and longer than that of other gorilla species, enables them to live in colder temperatures. Gorillas can be identified by nose prints unique to each individual',
@@ -10,7 +10,7 @@ module.exports = [{
 {
   name: 'Okapi',
   species_name: 'Okapia Johnstoni',
-  habitat: 'Rainforests',
+  habitat: 4,
   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Saint-Aignan_%28Loir-et-Cher%29._Okapi.jpg/220px-Saint-Aignan_%28Loir-et-Cher%29._Okapi.jpg',
   icon_url: 'https://image.flaticon.com/icons/png/128/166/166727.png',
   description: 'It has a long neck, and large and flexible ears. The coat is a chocolate to reddish brown, in sharp contrast to the white horizontal stripes and rings on the legs, and the white ankles. The distinctive stripes resemble those of a zebra. These features serve as an effective camouflage amidst dense vegetation.',
@@ -19,7 +19,7 @@ module.exports = [{
 {
   name: 'Two-Toed Sloth',
   species_name: 'Choloepus hoffmanni',
-  habitat: 'Rainforests',
+  habitat: 4,
   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Bicho-pregui%C3%A7a_3.jpg/220px-Bicho-pregui%C3%A7a_3.jpg',
   icon_url: 'https://image.flaticon.com/icons/png/128/3251/3251154.png',
   description: 'The name "sloth" means "lazy", but the slow movements of this animal are actually an adaptation for surviving on a low-energy diet of leaves. These sloths have half the metabolic rate of a typical mammal of the same size. Sloths have very poor eyesight and hearing, and rely almost entirely on their senses of touch and smell to find food.',
@@ -28,7 +28,7 @@ module.exports = [{
 {
   name: 'Capybara',
   species_name: 'Hydrochoerus Hydrochaeris',
-  habitat: 'Rainforests',
+  habitat: 4,
   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/220px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG',
   icon_url: 'https://image.flaticon.com/icons/png/128/2417/2417619.png',
   description: 'Capybaras are semiaquatic mammals found throughout almost all countries of South America except Chile. They live in densely forested areas near bodies of water, such as lakes, rivers, swamps, ponds, and marshes, as well as flooded savannah and along rivers in the tropical rainforest.',
@@ -37,7 +37,7 @@ module.exports = [{
 {
   name: 'Green Anaconda',
   species_name: 'Eunectes Murinus',
-  habitat: 'Rainforests',
+  habitat: 4,
   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/01-COBRA-SUCURI-3M-WAGNER-MEIER_MG_2458.JPG/220px-01-COBRA-SUCURI-3M-WAGNER-MEIER_MG_2458.JPG',
   icon_url: 'https://image.flaticon.com/icons/png/128/3196/3196011.png',
   description: 'Although the name applies to a group of snakes, it is often used to refer only to one species, in particular, the common or green anaconda, which is the largest snake in the world by weight, and the second longest.',
@@ -46,7 +46,7 @@ module.exports = [{
 {
   name: 'Red-Handed Howler',
   species_name: 'Alouatta belzebul',
-  habitat: 'Rainforests',
+  habitat_id: 4,
   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Alouatta_belzebul_Tucurui_PA_Brazil.jpg/220px-Alouatta_belzebul_Tucurui_PA_Brazil.jpg',
   icon_url: 'https://image.flaticon.com/icons/png/128/2417/2417553.png',
   description: 'It lives in the biomes of Amazonian and Atlantic forests which consist of dense forest separated by drier areas. These animals can live up to 20 years, and do not mature until later in their lives. They have a long gestation period, and generally reproduce more slowly than similar-sized mammals of other species. From the births that have been recorded, red-handed howlers have quick, quiet births during the day when they are normally active.',
