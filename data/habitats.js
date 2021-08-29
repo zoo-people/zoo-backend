22148248fbc02bed4f76560a2e3e64a6cd18afc4
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-29 14:45:15 -0700

    added image urls to habitats

--- data/habitats.js
+++ data/habitats.js
@@ -1,12 +1,12 @@
 module.exports = [
   { name: 'Polar',
-    image: '' },
+    image: 'https://www.maxpixel.net/static/photo/1x/Ice-Arctic-Landscape-Scenery-Full-Moon-Nightsky-152720.png' },
   { name: 'Desert',
-    image: ''  },
+    image: 'https://cdn.pixabay.com/photo/2020/04/30/10/08/desert-5112222_1280.png'  },
   { name: 'Grassland',
-    image: ''  },
+    image: 'https://st3.depositphotos.com/29856464/35717/v/600/depositphotos_357176076-stock-video-animation-clouds-moving-wind-field.jpg'  },
   { name: 'Rain Forest',
-    image: ''  },
+    image: 'https://t4.ftcdn.net/jpg/01/99/31/15/360_F_199311558_L9hwUDU5pd4Mc5n5EYA0J90LlvM79v9K.jpg' },
   { name: 'Evergreen Forest',
-    image: ''  }
+    image: 'https://www.maxpixel.net/static/photo/1x/Landscape-Nature-Background-Wallpaper-Illustration-4685547.jpg'  }
 ];
