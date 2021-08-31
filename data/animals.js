9fa499ea849cfdf71a839ba938168ca1887818fd
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-31 09:38:01 -0700

    added to animal.js living room

--- data/animals.js
+++ data/animals.js
@@ -241,6 +241,32 @@ module.exports = [
   },
 
   {
+    name: 'Jaguar',
+    species_name: '(Panthera onca)',
+    habitat: 'Rainforests',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Standing_jaguar.jpg/220px-Standing_jaguar.jpg',
+    icon_url: '',
+    description: 'The adult jaguar is an apex predator, meaning it is at the top of the food chain and is not preyed upon in the wild. The jaguar has also been termed a keystone species, as it is assumed that it controls the population levels of prey such as herbivorous and granivorous mammals, and thus maintains the structural integrity of forest systems. However, accurately determining what effect species like the jaguar have on ecosystems is difficult, because data must be compared from its current habitats and regions where it is absent, and while controlling for the effects of human activity.',
+    diet: 'Carnivore' },
+  {
+    name: 'South-American Coati',
+    species_name: '(Nasua nasua)',
+    habitat: 'Rainforests',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Nasenbaer_Nasua_nasua_Zoo_Augsburg-04.jpg/220px-Nasenbaer_Nasua_nasua_Zoo_Augsburg-04.jpg',
+    icon_url: '',
+    description: 'South American coatis are diurnal animals, and live both on the ground and in trees. They are omnivorous, but primarily eat fruit, invertebrates, other small animals and bird eggs. They search for fruit in trees high in the canopy, and use their snouts to poke through crevices to find animal prey on the ground. They also search for animal prey by turning over rocks on the ground or ripping open logs with their claws.',
+    diet: 'Omnivores' },
+  
+  {
+    name: 'Keel-Billed Toucan',
+    species_name: '(Ramphastos sulfuratus)',
+    habitat: 'Rainforests',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Ramphastos_sulfuratus_-Belize_Zoo-6a-2c.jpg/220px-Ramphastos_sulfuratus_-Belize_Zoo-6a-2c.jpg',
+    icon_url: '',
+    description: 'Like many toucans, keel-billed toucans are very social birds, rarely seen alone. They fly in small flocks of approximately six to twelve individuals through lowland rainforests. Their flight is slow and undulating, consisting of rapid wing beats (six to ten), then a glide with the beak extending forward and dipping downward as though pulling the rest of the bird. Their feet are drawn up forward in flight. The flight distances are typically short.',
+    diet: 'Omnivores' },
+
+  {
     name: 'Beaver',
     species_name: '(Castor canadensis)',
     habitat_id: 5,
@@ -311,6 +337,52 @@ module.exports = [
             attacks on people have been attributed to animals suffering from rabies. Wolf attacks on humans are rare because wolves are relatively few, live away from 
             people, and have developed a fear of humans because of their experiences with hunters, ranchers, and shepherds.`,
     diet: 'Carnivore'
-  },  
+  }, 
+
+  { 
+    name: 'Border Collie',
+    species_name: '(Canis lupus familiaris)',
+    habitat_id: 6,
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/BORDER_COLLIE%2C_Simaro_Million_Dollar_Baby_%2824290879465%29_2.jpg/220px-BORDER_COLLIE%2C_Simaro_Million_Dollar_Baby_%2824290879465%29_2.jpg',
+    icon_url: '',
+    description: 'The border collie is descended from landrace collies, a type found widely in the British Isles. The name for the breed came from its probable place of origin along the Anglo-Scottish border. Mention of the "collie" or "colley" type first appeared toward the end of the 19th century, although the word "collie" is older than this and has its origin in the Scots language. It is also thought that the word "collie" comes from the old Celtic word for useful. Many of the best border collies today can be traced back to a dog known as Old Hemp.',
+    diet: 'Omnivore' 
+  },
+  { 
+    name: 'Tuxedo',
+    species_name: '(Bicolor Cat)',
+    habitat_id: 6,
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Socks-clinton.jpg/300px-Socks-clinton.jpg',
+    icon_url: '',
+    description: 'A tuxedo cat, or Felix cat, or Julius cat is a bicolor cat with a white and black coat. They are called "tuxedo" cats because they appear to be wearing the type of black tie formal wear commonly known in the United States and Canada as a tuxedo. The tuxedo pattern is not limited to the color black, but the name is typically reserved for black and white cats.',
+    diet: 'Omnivore' 
+  },
+  { 
+    name: 'Parrot',
+    species_name: '(Psittaciformes)',
+    habitat_id: 6,
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Macaw_parrot_sitting_on_a_tree_branch.jpg/220px-Macaw_parrot_sitting_on_a_tree_branch.jpg',
+    icon_url: '',
+    description: 'Parrots, along with ravens, crows, jays, and magpies, are among the most intelligent birds, and the ability of some species to imitate human speech enhances their popularity as pets. Trapping wild parrots for the pet trade, as well as hunting, habitat loss, and competition from invasive species, has diminished wild populations, with parrots being subjected to more exploitation than any other group of birds. Measures taken to conserve the habitats of some high-profile charismatic species have also protected many of the less charismatic species living in the same ecosystems.',
+    diet: 'Omnivore' 
+  },
+  { 
+    name: 'Ferret',
+    species_name: '(Mustela furo)',
+    habitat_id: 6,
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png',
+    icon_url: '',
+    description: 'For millennia, the main use of ferrets was for hunting, or "ferreting". With their long, lean build, and inquisitive nature, ferrets are very well equipped for getting down holes and chasing rodents, rabbits and moles out of their burrows. Caesar Augustus sent ferrets or mongooses (named viverrae by Plinius) to the Balearic Islands to control the rabbit plagues in 6 BC.',
+    diet: 'Omnivores' 
+  },
+  { 
+    name: 'Leopard Gecko',
+    species_name: '(Eublepharis macularius)',
+    habitat_id: 6,
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Eublepharis_macularius1.jpg/220px-Eublepharis_macularius1.jpg',
+    icon_url: '',
+    description: 'Common leopard geckos have predators such as snakes, foxes, and other large reptiles. Their keen sense of hearing and sight help them escape from them during the night. Along with their exceptional sight and hearing abilities, their skin helps camouflage themselves from their predators. Their sense of taste and smell also helps them with survival. They also stay in underground holes and burrows during the daytime, avoid the risk of getting eaten.',
+    diet: 'Insectivorous' 
+  }
 
 ];
